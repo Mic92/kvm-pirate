ccfce8662db909d54c41f48216d0edbac997a473
Jörg Thalheim <joerg@thalheim.io>
2021-01-13 11:47:12 +0100

    add mypy/black/flake8

--- default.nix
+++ default.nix
@@ -9,7 +9,19 @@ buildPythonPackage {
   propagatedBuildInputs = [
     linuxPackages_latest.bcc
   ];
+  preCheck = ''
+    echo -e "\x1b[32m## run black\x1b[0m"
+    LC_ALL=en_US.utf-8 black --check .
+    echo -e "\x1b[32m## run flake8\x1b[0m"
+    flake8 .
+    echo -e "\x1b[32m## run mypy\x1b[0m"
+    mypy --strict .
+
+  '';
   checkInputs = [
     pytestCheckHook
+    black
+    flake8
+    mypy
   ];
 }
