e9ed3fb2f0ecc1542019799b2faaf6d871a284ce
Jörg Thalheim <joerg@thalheim.io>
2021-01-28 14:10:27 +0100

    add helper scripts

--- scripts/attach-qemu.sh
+++ scripts/attach-qemu.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export PYTHONPATH="$DIR/..:$PYTHONPATH"
-python -m kvm_pirate $(pidof qemu-system-x86_64)
+python -m kvm_pirate "$(pidof qemu-system-x86_64)"
